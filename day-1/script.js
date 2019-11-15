9d421b9e9373d43d7fb8b32f6752e2f98aad04bc
ikram.tanjib <ikram.tanjib@gmail.com>
2019-11-16 00:50:43 +0600

    day 1 codes

---
+++ day-1/script.js
@@ -0,0 +1,22 @@
+// data types
+/*
+this is a
+comment
+*/
+let num = 3.3; // number data type
+let name = 'T'; // string
+let animal = true; // bool
+let person;
+
+
+// console.log(typeof person);
+
+let x = 5;
+let y = '6';
+
+// console.log(x + y);
+
+let a = 10;
+let b = 10;
+
+console.log(a <= b);
---
+++ day-1/syntax.js
@@ -0,0 +1,119 @@
+let contact = 123;
+// console.log(contact);
+
+// var num = 12;
+const xyx = 12;
+// console.log(xyz);
+
+
+// if - else conditional
+let age = 20;
+
+if (age >= 21) {
+  console.log("Adult");
+}
+else {
+  console.log("Child");
+}
+
+// if - else if - else
+let operator = true;
+let prefix = '011';
+
+if (operator) {
+  if (prefix === '018') {
+    console.log('Robi');
+  } else if (prefix === '017' || prefix === '013' || prefix === '011') {
+    console.log('Grameen');
+  } else if (prefix === '019') {
+    console.log('Banglalik');
+  } else if (prefix === '016') {
+    console.log('Airtel');
+  } else {
+    console.log("Not an operator");
+  }
+}
+
+// conditional with and/or operators
+age = 21
+
+let gender = 'male  ';
+
+if (age >= 21 && gender === 'female') {
+  console.log("She can enter");
+} else if (age >= 21 && gender === 'male') {
+  console.log("not permitted");
+} else {
+  console.log("Not allowed");
+}
+
+// while loop
+
+let counter = 0;
+
+while (counter < 10) {
+  console.log(counter);
+  counter = counter + 1;
+}
+
+console.log(counter);
+
+
+// do while loop
+let counter = 0;
+
+do {
+  console.log(counter);
+  counter = counter + 1;
+} while (counter < 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
