3be5e622da65b4db9ea881076881978b93aac767
ikram.tanjib <ikram.tanjib@gmail.com>
2019-11-22 20:58:03 +0600

    exercise no. 1 added

---
+++ day-2/exercise/ex_1.js
@@ -0,0 +1,14 @@
+/************************************
+ * You have to find out the string '0001001000'
+ * (saved in variable: `search_string`) from the list.
+ * Push the index of the string in the `loc` array.
+ * There is exactly 5 (FIVE) '0001001000' string in the list.
+ * You don't have worry about other files in the directory.
+ * Just write your codes in this file.
+ * Enjoy!
+ ********************************************/ 
+let search_string = '0001001000';
+let loc = [];
+
+// Here is the first element from `list` variable
+console.log(list[0]);
